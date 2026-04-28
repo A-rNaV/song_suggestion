151a0bdd0d71fb126ec3fb56f3628ffad2cfff80
A-rNaV <148805601+A-rNaV@users.noreply.github.com>
2026-04-29 03:12:37 +0530

    logo added

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -2,6 +2,7 @@ import React from "react";
 import SuggestionForm from "../components/SuggestionForm";
 import SuggestionList from "../components/SuggestionList";
 import { useSocket } from "../context/SocketContext";
+import octavesLogo from './octaves_logo.png';
 
 export default function HomePage() {
   const { userCount, isConnected } = useSocket();
@@ -11,7 +12,7 @@ export default function HomePage() {
       <header className="site-header">
         <div className="header-inner">
           <div className="logo">
-            <span className="logo-icon"><img src="octaves_logo.png" alt="Octaves Logo" /></span>
+            <span className="logo-icon"><img src={octavesLogo} alt="Octaves Logo" style={{ width: '40px', height: 'auto' }}/></span>
             <span className="logo-text">SongDrop</span>
           </div>
           <div className="live-badge">
